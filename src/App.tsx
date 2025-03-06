58787f169544215e13d08fd190fc9f9d55b9affb
klxcoder <klxcoder@gmail.com>
2025-03-06 21:31:45 +0700

    GridView accept tables as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import styles from './App.module.scss';
 import GridView from './components/GridView';
+import { Table } from './models/Table';
 
 function App() {
-
+  const [tables] = useState<Table[]>([
+  ]);
   return (
     <div className={styles.app}>
-      <GridView />
+      <GridView tables={tables} />
     </div>
   )
 }
--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -1,7 +1,12 @@
+import { Table } from '../models/Table';
 import styles from './Grid.module.scss';
 
-function GridView() {
-
+function GridView({
+  tables,
+}: {
+  tables: Table[],
+}) {
+  console.log(tables);
   return (
     <div className={styles.grid}>
       GridView
