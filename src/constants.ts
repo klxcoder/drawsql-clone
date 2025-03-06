dd9d1239fc63f90f21c383abe6daf866a41189a3
klxcoder <klxcoder@gmail.com>
2025-03-06 21:41:55 +0700

    rename constants for better readability

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Maximum table row
-export const X = 10;
+export const MAX_TABLE_ROWS = 10;
 
 // Maximum table column
-export const Y = 10;
+export const MAX_TABLE_COLS = 10;
 
 // The size of each cell in table
 export const CELL_SIZE = 10;
