f3638bba3ae5d46f4dbc6dd29bfc6a6c08f3da4b
klxcoder <klxcoder@gmail.com>
2025-03-06 21:24:50 +0700

    class Table have attribute rect

--- src/models/Table.ts
+++ src/models/Table.ts
@@ -1,10 +1,16 @@
+import { Rect } from "./Rect";
+
 export class Table {
   title: string;
+  rect: Rect;
   constructor({
     title,
+    rect,
   }: {
     title: string,
+    rect: Rect,
   }) {
     this.title = title;
+    this.rect = rect;
   }
 }
