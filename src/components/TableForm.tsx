29c2be4779ae1cbb4f9b9c2e1e186d2b93ced1e6
klxcoder <klxcoder@gmail.com>
2025-03-07 15:16:39 +0700

    TableForm css

--- src/components/TableForm.tsx
+++ src/components/TableForm.tsx
@@ -17,9 +17,30 @@ function TableForm({
           onChange={(e) => onChangeTableName(e.target.value)}
         />
       </div>
-      {table.columns.map((column, index) => (
-        <div key={index}>{column.name}</div>
-      ))}
+      <div className={styles.columns}>
+        {table.columns.map((column, index) => (
+          <div
+            className={styles.column}
+            key={index}
+          >
+            <input
+              className={styles.keyType}
+              value={column.keyType}
+              onChange={() => { }}
+            />
+            <input
+              className={styles.columnName}
+              value={column.name}
+              onChange={() => { }}
+            />
+            <input
+              className={styles.columnType}
+              value={column.columnType}
+              onChange={() => { }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
