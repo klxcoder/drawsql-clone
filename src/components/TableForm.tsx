815f6707a48ad6113c9d084e63875e1ee438580a
klxcoder <klxcoder@gmail.com>
2025-03-07 14:48:05 +0700

    TableForm

--- src/components/TableForm.tsx
+++ src/components/TableForm.tsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import styles from './TableForm.module.scss';
+import { Table } from '../models/Table';
 
 function TableForm() {
+  const [table, setTable] = useState<Table>(new Table({
+    name: '',
+    rowCol: {
+      row: 0,
+      col: 0,
+    },
+    columns: [],
+  }));
   return (
     <div className={styles.tableForm}>
-      TableForm
+      <div className={styles.tableName}>
+        <input
+          className={styles.tableName}
+          placeholder='table1'
+          value={table.name}
+          onChange={(e) => setTable(new Table({
+            name: e.target.value,
+            rowCol: {
+              row: table.rect.row,
+              col: table.rect.col,
+            },
+            columns: table.columns,
+          }))}
+        />
+      </div>
     </div>
   )
 }
