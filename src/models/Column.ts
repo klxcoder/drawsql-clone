93b09ee04b987752f636c9d76a8405addf4a9330
klxcoder <klxcoder@gmail.com>
2025-03-13 17:00:14 +0700

    Column

--- src/models/Column.ts
+++ src/models/Column.ts
@@ -1,9 +1,5 @@
-import { Data } from "./Data";
-
 export type ColumnData = {
   keyType: string, // "PK", "FK", ""
   name: string,
   columnType: string,
 }
-
-export class Column extends Data<ColumnData> { }
