26171500a2a178f8bb9bbd29f6598b86da3eed22
klxcoder <klxcoder@gmail.com>
2025-03-13 16:52:19 +0700

    WidthHeight

--- src/models/WidthHeight.ts
+++ src/models/WidthHeight.ts
@@ -5,25 +5,4 @@ export type WidthHeightData = {
   height: number
 }
 
-export class WidthHeight extends Data<WidthHeightData> {
-  public width: number;
-  public height: number;
-
-  public getData: () => WidthHeightData = () => {
-    return {
-      width: this.width,
-      height: this.height,
-    }
-  }
-
-  public setData(data: WidthHeightData) {
-    this.width = data.width;
-    this.height = data.height;
-  }
-
-  constructor(width: number, height: number) {
-    super();
-    this.width = width;
-    this.height = height;
-  }
-}
+export class WidthHeight extends Data<WidthHeightData> { }
