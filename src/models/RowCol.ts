2ad523664d7b3c3eb5febc1d6dc4b573c2ede7bf
klxcoder <klxcoder@gmail.com>
2025-03-13 16:51:46 +0700

    RowCol

--- src/models/RowCol.ts
+++ src/models/RowCol.ts
@@ -5,25 +5,4 @@ export type RowColData = {
   col: number
 }
 
-export class RowCol extends Data<RowColData> {
-  public row: number;
-  public col: number;
-
-  public getData(): RowColData {
-    return {
-      row: this.row,
-      col: this.col,
-    }
-  }
-
-  public setData(data: RowColData): void {
-    this.row = data.row;
-    this.col = data.col;
-  }
-
-  constructor(row: number, col: number) {
-    super();
-    this.row = row;
-    this.col = col;
-  }
-}
+export class RowCol extends Data<RowColData> { }
