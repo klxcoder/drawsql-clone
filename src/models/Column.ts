0e18f3228391ce626e618018a08c34b8d6fa3c42
klxcoder <klxcoder@gmail.com>
2025-03-13 16:51:12 +0700

    Column

--- src/models/Column.ts
+++ src/models/Column.ts
@@ -1,32 +1,9 @@
 import { Data } from "./Data";
 
 export type ColumnData = {
-  keyType: string,
+  keyType: string, // "PK", "FK", ""
   name: string,
   columnType: string,
 }
 
-export class Column extends Data<ColumnData> {
-  public keyType: string = ''; // "PK", "FK", ""
-  public name: string = '';
-  public columnType: string = '';
-
-  public getData(): ColumnData {
-    return {
-      keyType: this.keyType,
-      name: this.name,
-      columnType: this.columnType,
-    }
-  }
-
-  public setData(data: ColumnData) {
-    this.keyType = data.keyType;
-    this.name = data.name;
-    this.columnType = data.columnType;
-  }
-
-  constructor(columnData: ColumnData) {
-    super();
-    this.setData(columnData);
-  }
-}
+export class Column extends Data<ColumnData> { }
